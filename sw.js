cdff9bf398b5b64cee45a98162f76c5a4bf1bc2f
ilya <ilya.tchivilev@dataprocessors.com.au>
2021-05-14 13:46:34 +1000

    sw interaction - not sending yet, but poised to do so

--- sw.js
+++ sw.js
@@ -5,3 +5,11 @@ self.addEventListener('install', async event => {
   self.addEventListener('fetch', async event => {
     console.log('fetch event')
   });
+
+
+  self.addEventListener('message', event => {
+    // event is an ExtendableMessageEvent object
+    console.log(`The client sent me a message: ${event.data}`);
+  
+    event.source.postMessage(`Hi client, you sent me ${event.data}`);
+  });
